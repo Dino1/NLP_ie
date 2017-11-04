5c5218cdcc95bed4a47e47d1641cca628d7c6904
jacobcluke <jacobcluke@gmail.com>
2017-11-03 19:18:50 -0600

    Uploaded the stuff we did today

---
+++ src/final_project/Infoextract.java
@@ -0,0 +1,114 @@
+package final_project;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.ling.SentenceUtils;
+import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
+import edu.stanford.nlp.parser.shiftreduce.ShiftReduceParser;
+import edu.stanford.nlp.process.CoreLabelTokenFactory;
+import edu.stanford.nlp.process.DocumentPreprocessor;
+import edu.stanford.nlp.process.PTBTokenizer;
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
+import edu.stanford.nlp.trees.Constituent;
+import edu.stanford.nlp.trees.Tree;
+
+/**
+ * CS 5340
+ * Final Project
+ * 
+ * @author Jacob Luke and Bernard Serbinowski
+ *
+ */
+public class Infoextract {
+
+	public static void main(String[] args) {
+		
+		Scanner input_scanner = null;
+		PrintWriter  writer = null;
+		//try {input_scanner = new Scanner(new File(args[0]));}
+		try {
+			input_scanner = new Scanner(new File("input.txt"));
+			writer = new PrintWriter("input.txt.template");			
+		}
+		catch (FileNotFoundException e) {e.printStackTrace();}
+		
+		MaxentTagger tagger = new MaxentTagger("./english-left3words-distsim.tagger");
+		LexicalizedParser parsnip = LexicalizedParser.loadModel();
+		
+		// Go through all articles
+		String next_article_name = "";
+		String current_article_name = "";
+		boolean done_with_stuff=false;
+		while(!done_with_stuff){
+			String the_article="";
+			boolean next_article_found=false;
+			while(input_scanner.hasNextLine() &&! next_article_found){
+				String next_line = input_scanner.nextLine();
+				
+				if(next_line.matches("(DEV-MUC3-[0-9]{4}.*)|(TST1-MUC3-[0-9]{4}.*)|(TST2-MUC4-[0-9]{4}.*)")){
+					next_article_found=true;
+					current_article_name = next_article_name;
+					next_article_name = next_line;
+				}
+				else
+					the_article += next_line;
+			}
+			if(!next_article_found)
+				done_with_stuff = true;
+			
+			if(!the_article.equals("")){
+				
+				// GET THE ID
+				String ID = current_article_name.substring(0, 14);
+				if(ID.charAt(0) == 'D')
+					ID = ID.substring(0, 13);
+				
+				// SPLIT THE ARTICLE INTO SENTENCES
+				Reader reader = new StringReader(the_article);
+				DocumentPreprocessor dp = new DocumentPreprocessor(reader);				
+				ArrayList<Tree> tag_trees = new ArrayList<Tree>();
+				
+				for (List<HasWord> sentence : dp)
+					tag_trees.add(parsnip.apply(tagger.tagSentence(sentence)));
+
+				for(Tree t : tag_trees)
+					t.constituents();
+				
+				// OUTPUT THE STUFFS		
+				writer.println("ID:             " + ID);
+				writer.println("INCIDENT:       " + "ATTACK");
+				
+				writer.println("WEAPON:         " + "ARTILLERY FIRE");
+				writer.println("                " + "EXPLOSIONS");
+				
+				writer.println("PERP INDIV:     " + "-");
+				writer.println("                " + "-");
+				
+				writer.println("PERP ORG:       " + "-");
+				writer.println("                " + "-");
+				
+				writer.println("TARGET:         " + "-");
+				writer.println("                " + "-");
+				
+				writer.println("VICTIM:         " + "-");
+				writer.println("                " + "-");
+				
+				writer.println("");
+			}			
+		}
+		
+		writer.close();
+	}
+}
